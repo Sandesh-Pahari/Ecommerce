29ff2a4302d08eec837db10814cdd22b467a4fd0
Sandesh-Pahari <sandeshpahari05@gmail.com>
2024-06-09 17:56:49 +0545

    created login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Livewire\Pages\Auth\Login;
+use App\Livewire\Pages\Auth\Register;
 use App\Livewire\Pages\Home;
 use App\Livewire\Pages\Shop;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',Home::class)->name('home');
 Route::get('/shop',Shop::class)->name('shop'); 
+Route::get('/login',Login::class)->name('login');
+Route::get('/register',Register::class)->name('register');
