05d48a9f08dc125626f594132a2b37f67ad54390
Sandesh-Pahari <sandeshpahari05@gmail.com>
2024-06-09 20:05:46 +0545

    [FEAT]: created admin dashboard layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Pages\Admin\Dashboard;
 use App\Livewire\Pages\Auth\Login;
 use App\Livewire\Pages\Auth\Register;
 use App\Livewire\Pages\Checkout;
@@ -8,9 +9,15 @@ use App\Livewire\Pages\PaymentOption;
 use App\Livewire\Pages\Shop;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',Home::class)->name('home');
-Route::get('/shop',Shop::class)->name('shop'); 
-Route::get('/checkout',Checkout::class)->name('checkout');
-Route::get('/payment',PaymentOption::class)->name('payment');
-Route::get('/login',Login::class)->name('login');
-Route::get('/register',Register::class)->name('register');
+
+Route::get('/', Home::class)->name('home');
+Route::get('/shop', Shop::class)->name('shop');
+Route::get('/checkout', Checkout::class)->name('checkout');
+Route::get('/payment', PaymentOption::class)->name('payment');
+Route::get('/login', Login::class)->name('login');
+Route::get('/register', Register::class)->name('register');
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', Dashboard::class)->name('dashboard');
+});
